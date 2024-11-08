66d5064bca37aab7d5d70eac3a939ee723a3c9b0
Ticoo <JLRGeorge.Lo@gmail.com>
2024-11-08 09:13:35 +0000

    slime moves

--- socials-page/src/components/Slime.tsx
+++ socials-page/src/components/Slime.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react"
 import { slime } from "./RenderSlimes"
 
 
 const Slime = ({ name, colour, tag }: slime) => {
+
+
+    const [position, setPosition] = useState(0)
+
+    const surfaceWidth = window.innerWidth
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setPosition((prev) => (prev + 1) % surfaceWidth)
+        }, 75)
+
+        return () => clearInterval(interval)
+    },[])
 
 
     const img: string = new URL(`../images/slime.png`, import.meta.url).href
@@ -9,7 +23,11 @@ const Slime = ({ name, colour, tag }: slime) => {
     const content = (
         <>
 
-            <div className="div__slime_placeable">
+            <div className="div__slime_placeable"
+            style={{
+                position: 'absolute',
+                left: position,
+            }}>
                 <p className="p__slime_name">{name}</p>
                 <img src={img} alt="slime" className="img__slime"
                     style={{
